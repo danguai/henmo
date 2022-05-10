f094261c6c6f934b7be246382bdfbc38d3b6ad26
Daniel Blanco <db@d-blanco.com>
2022-05-10 17:56:57 -0400

    LOGIN Page DONE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -31,11 +31,11 @@ function App() {
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>
-        <Route path='/sign-up' exact={true}>
+        <Route path='/signup' exact={true}>
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
+          <UsersList />
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+
+import './LoginForm.css';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +33,54 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div id='login__page'>
+      <div className='henmo__logo'>
+        <img src='/static/henmo-logo.png' alt='henmo-logo' />
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
+      <div className='login__form__container'>
+        <div className='forms__name'>
+          LOGIN
+        </div>
+        <form onSubmit={onLogin}>
+          <div className='forms__inputs__format'>
+            <label className='forms__label' htmlFor='email'>EMAIL</label>
+            <input
+              className='forms__input'
+              name='email'
+              type='text'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className='forms__inputs__format'>
+            <label className='forms__label' htmlFor='password'>PASSWORD</label>
+            <input
+              className='forms__input'
+              name='password'
+              type='password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+          <div className='errors__login'>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className='login__or__signup'>
+            <button className='red__button__basic login__btn__size' type='submit'>LOG IN</button>
+            <div className='or__signup'>
+              <div className='or'>
+                OR
+              </div>
+              <div>
+                <Link className='link__to__other__auth' to='/signup'>SIGN UP</Link>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
