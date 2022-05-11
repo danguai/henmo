201e87b4f39616eac7ddbf3a58a4bfe0dc04a288
Daniel Blanco <db@d-blanco.com>
2022-05-11 14:17:08 -0400

    constants and actions - DONE

---
+++ frontend/src/store/outgoing.js
@@ -0,0 +1,112 @@
+// OUTGOING CONSTANTS
+const CREATE_OUTGOING = 'outgoings/CREATE_OUTGOING';
+const READ_ALL_OUTGOINGS = 'outgoings/READ_ALL_OUTGOINGS';
+const READ_ONE_OUTGOING = 'outgoings/READ_ONE_OUTGOING';
+const UPDATE_OUTGOING = 'outgoings/UPDATE_OUTGOING';
+const DELETE_OUTGOING = 'outgoings/DELETE_OUTGOING';
+
+// OUTGOING ACTIONS
+const createOutgoingAction = outgoing => ({
+    type: CREATE_OUTGOING,
+    outgoing
+});
+
+const readAllOutgoingsAction = outgoings => ({
+    type: READ_ALL_OUTGOINGS,
+    outgoings
+});
+
+const readOneOutgoingAction = outgoing => ({
+    type: READ_ONE_OUTGOING,
+    outgoing
+});
+
+const updateOutgoingAction = outgoing => ({
+    type: UPDATE_OUTGOING,
+    outgoing
+});
+
+const deleteOutgoingAction = outgoing => ({
+    type: DELETE_OUTGOING,
+    outgoing
+});
+
+// OUTGOING THUNKS
+// C R E A T E
+export const createOutgoing = outgoing => async dispatch => {
+    const response = await fetch('api/outgoings/new-outgoing', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(outgoing)
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        await dispatch(createOutgoingAction(data));
+        return data;
+    } else {
+        console.log(data.errors);
+    }
+};
+
+// R E A D   A L L
+export const readAllOutgoings = () => async dispatch => {
+    const response = await fetch('/api/outgoings/');
+
+    const data = await response.json();
+
+    if (response.ok) {
+        await dispatch(readAllOutgoingsAction(data.outgoings));
+        return data;
+    } else {
+        console.log(data.errors);
+    }
+};
+
+// R E A D   O N E
+export const readOneOutgoing = id => async dispatch => {
+    const response = await fetch(`/api/outgoings/${id}`);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        await dispatch(readOneOutgoingAction(data));
+        return data;
+    } else {
+        console.log(data.errors);
+    }
+};
+
+// U P D A T E
+export const updateOutgoing = outgoing => async dispatch => {
+    const response = await fetch(`/api/outgoings/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(outgoing)
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        await dispatch(updateOutgoingAction(data));
+        return data;
+    } else {
+        console.log(data.errors);
+    }
+};
+
+// D E L E T E
+export const deleteOutgoing = outgoing => async dispatch => {
+    const response = await fetch(`/api/outgoings/${outgoing.id}`, {
+        method: 'DELETE'
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        await dispatch(deleteOutgoingAction(data));
+    } else {
+        console.log(data.errors);
+    }
+};
