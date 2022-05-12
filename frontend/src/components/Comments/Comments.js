06b583a609cc119c1a7ede98a2d316631cf55508
Daniel Blanco <db@d-blanco.com>
2022-05-12 19:42:50 -0400

    create comment - DONE

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink, useHistory } from 'react-router-dom';
 
 import Modal from '../Modal/Modal';
-import { readAllComments } from '../../store/comment';
+import { createComment, readAllComments, updateComment, deleteComment } from '../../store/comment';
 import { UserIcon } from '../UserIcons/UserIcons';
 import User from '../Users/User';
 
@@ -19,15 +19,25 @@ const Comments = ({ approvedTran }) => {
     const sessionUser = useSelector(state => state.session?.user);
     const allComments = useSelector(state => state.comment);
 
-    const theseComments = [];
+    const [message, setMessage] = useState('');
+    // const [editMessage, setEditMessage] = useState(message);
 
+    const theseComments = [];
     Object.values(allComments).forEach(comment => {
         if (comment?.outgoing_id === approvedTran?.id) {
             theseComments.push(comment)
         }
     });
-    console.log(theseComments);
 
+    const addComment = async () => {
+        const newComment = {
+            user_id: sessionUser.id,
+            outgoing_id: approvedTran.id,
+            message
+        };
+        await dispatch(createComment(newComment));
+        setMessage('');
+    };
 
     useEffect(() => {
         dispatch(readAllComments());
@@ -40,10 +50,31 @@ const Comments = ({ approvedTran }) => {
             <div className='approved__comment__title'>
                 COMMENTS
             </div>
+            <div className='next__comment'>
+                <div className='comments__image__users'>
+                    <UserIcon size={30} isNavIcon={true} />
+                </div>
+                <textarea
+                    className='textarea__input__comments'
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder='Add a comment...'
+                />
+                <div className='add__cancel__comments'>
+                    {/* {cardOwner && */}
+                    <button
+                        onClick={addComment}
+                        className=''>
+                        Add Comment
+                    </button>
+
+                </div>
+            </div>
             <div>
                 {theseComments.map((comment, i) =>
                     <div className='comment__box' key={i}>
-                        <div className='user__comment'>
+                        <div className='comments__image__users'>
                             <UserIcon id={comment.user_id} />
                         </div>
                         <div>
